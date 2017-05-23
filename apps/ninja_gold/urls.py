241f8b8902d720880c8ab6b3013ddaf9f5fb561e
Drew Pham <drewvpham@gmail.com>
2017-05-23 11:40:10 -0500

    got it working. need to do the activities

--- apps/ninja_gold/urls.py
+++ apps/ninja_gold/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^$', views.index),
+    url(r"^process_money/(?P<building>\w+)$", views.process_money),
+    url(r'^reset$', views.reset),
 ]
--- apps/ninja_gold/views.py
+++ apps/ninja_gold/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import random
 # Create your views here.
 def index(request):
+    if 'total'  and 'activities' not in request.session:
+        request.session['total']=0
+        request.session['activities']=[]
 
     return render(request, 'ninja_gold/index.html')
+
+
+def process_money(request, building):
+
+    if request.method=="POST":
+        if building=="farm":
+            gold=random.randint(10, 20)
+            request.session['total']+=gold
+        if building=="cave":
+            gold=random.randint(5, 10)
+            request.session['total']+=gold
+        if building=="house":
+            gold=random.randint(2, 5)
+            request.session['total']+=gold
+        if building=="casino":
+            gold=random.randint(-50, 50)
+            request.session['total']+=gold
+        return redirect('/')
+
+def reset(request):
+    if request.method=="POST":
+        request.session['total']=0
+        return redirect('/')
+
+    #
+    #     data=request.POST['building']
+    #     print data
+    #     if data=='farm':
+    #         request.session['gold']+=random.randint(10, 20)
+    #         print request.session['gold']
+    #     elif data=='cave':
+    #         request.session['gold']+=random.randint(5, 10)
+    #     elif data=='house':
+    #         request.session['gold']+=random.randint(2, 5)
+    #     elif data=='casino':
+    #         request.session['gold']+=random.randint(-50, -50)
+    # return redirect('/')
